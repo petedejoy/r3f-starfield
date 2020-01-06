cbcd7d8a8b5374be20acd542540806f4c693a6da
Pete DeJoy <pete@astronomer.io>
2020-01-05 21:54:17 -0500

    change sun to wireframe

--- src/components/Lights/index.js
+++ src/components/Lights/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import logo from "../../logo.svg";
+import Img from 'react-image'
 
 export default () => {
   const FakeSphere = () => {
     return (
       <mesh>
         <sphereBufferGeometry args={[0.7, 30, 30]} attach="geometry" />
-        <meshBasicMaterial color='#fffff' attach="material" />
+        <meshBasicMaterial color='#fffff' attach="material" wireframe />
       </mesh>
     );
   };
+  
 
   return (
     <group>
